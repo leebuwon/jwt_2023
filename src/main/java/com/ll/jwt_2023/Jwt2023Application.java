731f7bb945afc4682cb5ed64dd318363f6115876
bw1611 <bw1611@naver.com>
2023-05-16 14:40:45 +0900

    init commit

---
+++ src/main/java/com/ll/jwt_2023/Jwt2023Application.java
@@ -0,0 +1,13 @@
+package com.ll.jwt_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jwt2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/jwt_2023/Jwt2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.jwt_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwt2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
